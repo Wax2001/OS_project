74f9e0eb0bcfc624342beb3c5781c4c6bdf30d1a
Wax2001 <shah.shah.2001.22@gmail.com>
2021-10-26 19:33:34 +0500

    milestone 3 completef

--- Client.py
+++ Client.py
@@ -1,79 +1,55 @@
 import os
 from socket import *
+from threading import Thread
 
-# HOST = '127.0.0.1'
-PORT = 8910
-BUF_SIZE = 64
+HOST = '127.0.0.1'
+SEND_PORT = 2021
+RECV_PORT = 2022
+BUF_SIZE = 1024
 status = True
 
-
-while status:
-
-    connection = False
-    command = input("Enter a command: ")
-    com_split = command.split()
-    com1 = com_split[0]
-
-    if com1 == 'connect' and len(com_split) == 3:
-        send = 'CONNECT {}'.format(com_split[2])
-        HOST = com_split[2]
-        print(HOST)
-        #connecting to server
-        # with socket(AF_INET, SOCK_STREAM) as s:
-        s = socket(AF_INET, SOCK_STREAM)
-        try:
-            s.connect((HOST, PORT))
-            s.send(f'CONNECT {com_split[1]}'.encode())
-            ans = s.recv(BUF_SIZE).decode()
-            if ans.split()[0] == 'Welcome':
-                connection = True
-            print(ans)
-        except Exception as e:
-            print('IP address is not valid or server is not responding')
-            print(e)
-            continue
-    elif com1 == 'quit':
-        status = False
-
-    else:
-        print("Error: You are not connected to server")
-
-    while connection:
-
+def sending_thread(socket):
+    while True:
+        global status
         command = input("Enter a command: ")
         com_split = command.split()
         com1 = com_split[0]
 
-
         if com1 == 'disconnect':
-            s.send('DISCONNECT'.encode())
-            print('Disconnected')
-            connection = False
-            s.close()
+            socket.send('DISCONNECT'.encode())
+            if socket.recv(BUF_SIZE).decode() == 'OK':
+                print('Disconnected')
+                socket.close()
+                break
+            else:
+                print('Disconnection error')
 
         elif com1 == 'quit':
-            s.send('QUIT'.encode())
-            status = False
-            connection = False
+            socket.send('QUIT'.encode())
+            if socket.recv(BUF_SIZE).decode() == 'OK':
+                print('Quitting')
+                socket.close()
+                status = False
+                break
+            else:
+                print('Quit error')
 
         elif com1 == 'lu':
             # print('LU')
-            s.send('LU'.encode())
-            print(s.recv(BUF_SIZE).decode())
+            socket.send('LU'.encode())
+            print(socket.recv(BUF_SIZE).decode())
 
-        elif com1 == 'send':
-            if com_split[1] == 'not_online':
-                print('Error: User is not online')
-                continue
+        elif com1 == 'send' and len(com_split) > 2:
+
             msg = command[command.find('“') + 1:command.find('”')]
-            send = 'MESSAGE {}\n{} {}'.format(com_split[1], len(msg), msg)
-            print(send)
+            send_msg = 'MESSAGE {}\n{} {}'.format(com_split[1], len(msg), msg)
+            socket.send(send_msg.encode())
 
         elif com1 == 'lf':
-            s.send('LF'.encode())
-            print(s.recv(BUF_SIZE).decode())
+            socket.send('LF'.encode())
+            print(socket.recv(BUF_SIZE).decode())
 
-        #maybe create functions to read and write for using them in over- and appendfiles commands
+        # maybe create functions to read and write for using them in over- and append files commands
         elif com1 == 'read':
             if com_split[1] == 'already_exists':
                 print('Error: Client already contains that file')
@@ -86,7 +62,7 @@ while status:
             if com_split[1] == 'already_exists':
                 print('Error: Server already contains that file')
                 continue
-            send1 = 'WRITE {}'.format(com_split[1])
+            send = 'WRITE {}'.format(com_split[1])
             print(send)
             # wait for response
             # file_size = os.path.getsize('path')
@@ -106,5 +82,61 @@ while status:
 
         else:
             print("Error: Command is wrong or non_existent")
+
+
+def receiving_thread():
+    with socket(AF_INET, SOCK_STREAM) as rs:
+        rs.bind((HOST, RECV_PORT))
+        rs.listen()
+        print('\n', (HOST, RECV_PORT))
+        serv_sock, serv_addr = rs.accept()
+
+        print('\nconn is on')
+        while True:
+            print('waiting for messages')
+            message = serv_sock.recv(BUF_SIZE).decode()
+            if message:
+                print(message)
+            else:
+                break
+
+
+while status:
+    command = input("Enter a command: ")
+    com_split = command.split()
+
+    if com_split[0] == 'connect' and len(com_split) == 3:
+        # send = 'CONNECT {}'.format(com_split[2])
+        ip = com_split[2]
+        print(ip)
+        # connecting to server
+        # with socket(AF_INET, SOCK_STREAM) as s:
+        s = socket(AF_INET, SOCK_STREAM)
+        try:
+            s.connect((ip, SEND_PORT))
+            s.send(f'CONNECT {com_split[1]}'.encode())
+            ans = s.recv(BUF_SIZE).decode()
+            print(ans)
+        except Exception as e:
+            print('IP address is not valid or server is not responding')
+            print(e)
+            continue
+
+        Thread(target=receiving_thread, daemon=True).start()
+        st = Thread(target=sending_thread, args=(s,))
+        # rt = Thread(target=receiving_thread)
+        st.start()
+        # rt.start()
+        print('here1')
+        st.join()
+        # rt.join()
+        print('here2')
+
+
+    elif com_split[0] == 'quit':
+        status = False
+
+    else:
+        print("Error: You are not connected to server")
 
 print('Connections are terminated and program is closed')
--- Server.py
+++ Server.py
@@ -1,36 +1,64 @@
 from socket import *
+import time
 
 HOST = '127.0.0.1'
-PORT = 8910
-BUF_SIZE = 64
+# HOST = '192.168.31.15'
+PORT = 2021
+RECV_PORT = 2022
+BUF_SIZE = 1024
+con_recv = True
 
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen()
-
 while True:
     print('wait')
-    try:
-        clientsoclet, address = s.accept()
-        name_check = clientsoclet.recv(BUF_SIZE).decode().split()[1]
-        connection = False
-        if name_check == 'sh':
-            clientsoclet.send('Name is taken'.encode())
-        else:
-            clientsoclet.send(f'Welcome {name_check}'.encode())
-            connection = True
-            print('accepted')
-        while connection:
-            command = clientsoclet.recv(BUF_SIZE).decode()
+    clientsocket, address = s.accept()
+    print(clientsocket)
+    name_check = clientsocket.recv(BUF_SIZE).decode().split()[1]
+    # if name_check:
+    #     print(name_check, '/', address[0], '/', type(address[0]))
+    # else:
+    #     print('ffq')
+    clientsocket.send('OK'.encode())
+
+    recv_sock = socket(AF_INET, SOCK_STREAM)
+
+    connection = True
+    while connection:
+        # try:
+        #     print((address[0], RECV_PORT))
+        #     time.sleep(1)
+        #     recv_sock.connect((address[0], RECV_PORT))
+        #     print('connected to client receiver')
+        #     recv_sock.send('hi recv sock'.encode())
+        #
+        # except Exception as e:
+        #     print(f'Connection to Clients recv socket is failed {e} ')
+        #     break
+        command = clientsocket.recv(BUF_SIZE).decode()
+        if command:
             print(command)
             com1 = command.split()[0]
             if com1 == 'LU':
-                clientsoclet.send("Server's LU".encode())
-            elif com1 == 'QUIT' or com1 == 'DISCONNECT':
+                clientsocket.send("Server's LU".encode())
+            elif com1 == 'DISCONNECT' or com1 == "QUIT":
+                clientsocket.send('OK'.encode())
+                clientsocket.close()
+                recv_sock.close()
+                con_recv = True
                 break
+            elif com1 == 'MESSAGE':
+                print('Sending massage to {}'.format(command.split()[1]))
+                if con_recv:
+                    recv_sock.connect((address[0], RECV_PORT))
+                    con_recv = False
+                msg = command.replace(command.split()[1], '')
+                # print('connected to client receiver')
+                recv_sock.send(msg.encode())
             else:
-                clientsoclet.send("Comm not added".encode())
-            clientsoclet.close()
-    except KeyboardInterrupt:
-        print('\nServer closed')
-        break
+                clientsocket.send("Comm not added".encode())
+        else:
+            connection = False
+
+s.close()
